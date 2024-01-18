2c9c33377473e0ef6078bb6f1830dbc6b2cbb010
Guillaume CHARLIER <guillaume.charlier@cfecgc-inova.fr>
2024-01-18 18:56:49 +0100

    rails new

--- Gemfile
+++ Gemfile
@@ -4,9 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "3.1.2"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem "rails", "~> 7.0.2", ">= 7.0.2.3"
-
-gem "simple_form"
+gem "rails", "~> 7.0.8"
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
@@ -63,5 +61,3 @@ group :development do
   # gem "spring"
 end
 
-gem 'rspec-rails', group: [ :test ]
-gem 'rails-controller-testing', group: [ :test ]
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_16_202244) do
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.integer "stars"
+    t.string "address"
+    t.integer "phone_number"
+    t.string "category"
+    t.string "reviews"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "content"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
